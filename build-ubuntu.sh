9f36df146db3e766721d22634841e0976ff44c45
Jose Fernando Álvarez Romero <joseferalvarezromero@gmail.com>
2024-12-09 19:48:46 +0100

    frontend react added

---
+++ build-ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Load .env file
+if [ -f .env ]; then
+  source .env
+else
+  echo "The .env file doesnt exist"
+  exit 1
+fi
+
+#Login to github
+docker login --username $GHCR_USER --password $GHCR_PASSWORD ghcr.io
+
+#Build the image
+docker build . -t $GHCR_UBUNTU_PATH
+
+#Push the image to github
+docker push $GHCR_UBUNTU_PATH
---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
+import Logo from "../public/logo.svg"
+import "./App.css"
+
+function App() {
+  const [markdown, setMarkdown] = useState("");
+  const [html, setHtml] = useState("");
+  const [loader, setLoader] = useState(0);
+
+  useEffect(() => {
+    setHtml(DOMPurify.sanitize(marked.parse(markdown)));
+  }, [markdown]);
+
+  const getPdfByUrl = async (event) => {
+    event.preventDefault();
+
+    setLoader(1);
+
+    const res = await fetch("http://localhost:8000/api/v1/url/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({
+        url: event.target.url.value
+      })
+    });
+
+    const data = await res.json();
+    setMarkdown(data.file);
+
+    setLoader(0);
+  }
+  
+  return (
+    <>
+      <div className='converter'>
+        <div className='controls'>
+          <div className='title'>
+            <img className='logo' src={Logo}></img>
+            pdf2md
+          </div>
+          <form className='form-url' onSubmit={(e) => getPdfByUrl(e)}>
+            <input type='text' placeholder='Introduce una url' name='url'></input>
+            <button type='submit'>Extraer</button>
+          </form>
+        </div>
+
+        <div className='content'>
+          {!html && !loader && <div className='input-text'>Introduce una url para extraer su contenido</div>}
+          {loader ?
+            <div className='loader-container'>
+              <div className="loader"></div>
+            </div>
+          : ""}
+          {html && <div dangerouslySetInnerHTML={{__html: html}}></div>}
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
